f411db8d9140c6c8bf96ea8b64088d514ea8c74c
Leyth Toubassy <lizardman1028@gmail.com>
2025-11-10 16:50:11 -0800

    more of the previous commit

--- overflowUnityProj/Assets/PlayerMovement.cs
+++ overflowUnityProj/Assets/PlayerMovement.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour {
+  [SerializeField] 
+  float groundVelocity = 5f;
+  
+  [SerializeField]
+  Rigidbody2D rigidBody2D;
+  private void Update() {
+    if (Input.GetKeyDown(KeyCode.A)) {
+      var velocity = new Vector2(-groundVelocity, 0);
+      rigidBody2D.velocity += velocity;
+    } else if (Input.GetKeyUp(KeyCode.A)) {
+      var velocity = new Vector2(-groundVelocity, 0);
+      rigidBody2D.velocity -= velocity;
+      //rigidBody2D.AddForce(force, ForceMode2D.Impulse);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    if (Input.GetKeyDown(KeyCode.D)) {
+      rigidBody2D.velocity += new Vector2(groundVelocity, 0);
+    } else if (Input.GetKeyUp(KeyCode.D)) {
+      rigidBody2D.velocity -= new Vector2(groundVelocity, 0);
     }
+    
+    
+  }
 }
