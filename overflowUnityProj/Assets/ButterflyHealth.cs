d20db8ef4778423455dabdde01ae2621b6cf8f21
bachigaga <105132568+bachigaga@users.noreply.github.com>
2025-11-18 21:58:23 -0800

    Added Juice, Digit Particle Explosion, Sound effect

--- overflowUnityProj/Assets/ButterflyHealth.cs
+++ overflowUnityProj/Assets/ButterflyHealth.cs
@@ -5,11 +5,21 @@ public class ButterflyHealth : MonoBehaviour
 {
     public int maxHealth = 10;  // maximum HP
     public int health = 1;     // current HP, starts at 0
-    
+    public AudioSource burstSound;
+    public float startingPitch = 1f;
+    public float pitchIncreaseAmount = 0.1f;
+    private float currentPitch;
+
     //TEMP!!!
     [SerializeField]
     private FloatLevelController floatLevelController;
-    
+
+    void Start()
+    {
+        currentPitch = startingPitch;
+        burstSound.pitch = currentPitch;
+    }
+
     public void AddHealth(int amount)
     {
         health += amount;
@@ -18,6 +28,15 @@ public class ButterflyHealth : MonoBehaviour
         if (health >= maxHealth) {
           health = 1;
           floatLevelController.raiseWater();
+          burstSound.pitch = currentPitch;
+          burstSound.Play();  
+          currentPitch = Mathf.Min(currentPitch + pitchIncreaseAmount);
+
+          DigitBurst digitBurst = GetComponent<DigitBurst>();
+          if (digitBurst != null)
+          {
+              digitBurst.SpawnDigitBurst(transform.position);
+          }
         }
     }
 }
